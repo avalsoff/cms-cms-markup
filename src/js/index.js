a7c6eec26f3bba135ca6363318238f9d91d86040
Vyacheslav <avalsoff@gmail.com>
2018-12-26 15:55:50 +1000

    Finish edit page

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import Choices from 'choices.js'
 
 import dropdown from './dropdown'
 import multiImages from './multi-images'
 
-
+new Choices(document.querySelector('#choices'))
 ClassicEditor.create( document.querySelector('#ckeditor') )
 
 dropdown('dropdown')
